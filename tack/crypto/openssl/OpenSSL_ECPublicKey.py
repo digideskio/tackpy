0471e7bee71ac30484374d7aaf7ac03ce7a8b816
Trevor <trevp@trevp.net>
2012-08-30 18:51:04 -0400

    Roll back base32 l->8

--- tack/crypto/openssl/OpenSSL_ECPublicKey.py
+++ tack/crypto/openssl/OpenSSL_ECPublicKey.py
@@ -57,7 +57,7 @@ class OpenSSL_ECPublicKey:
     def getFingerprint(self):
         digest = Digest.SHA256(self.rawPublicKey)
         assert(len(digest) == 32)
-        s = b2a_base32(digest).lower()[:25].replace('l', '8')
+        s = b2a_base32(digest).lower()[:25]
         return "%s.%s.%s.%s.%s" % (s[:5],s[5:10],s[10:15],s[15:20],s[20:25])
 
     def _constructEcFromRawKey(self, rawPublicKey):
